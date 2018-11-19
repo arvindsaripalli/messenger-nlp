9b49ff661c4a560c66b0c9fec01827bf2b9fdd05
Arvind Saripalli <mail2asrs@gmail.com>
2018-11-18 23:04:30 -0800

    Writing new Message Downloader class. Wrote in better arg parsing to include multiple names and thread ids.

--- get_messages.py
+++ get_messages.py
@@ -3,26 +3,68 @@ import getpass
 import joblib
 import os
 from fbchat import Client
+from fbchat.models import *
+from datetime import datetime
 
+class MessageDownloader:
+    def __init__(self, args):
+        """
+            Authenticates the Message downloader.
+            args: Command line arguments.
+        """
+        self.names = args.names
+        self.thread_ids = args.ids
+        self.client = self.authenticate()
 
-def get_args():
-    parser = argparse.ArgumentParser(description='Program description.')
-    parser.add_argument('--name', required=True, help='Name of user conversation to get messages from')
-    parser.add_argument('--limit', default=30, help='Number of messages to be recorded')
+    def authenticate(self):
+        """
+            Returns an fbchat.Client object after signing into Messenger.
+        """
+        user = str(input("Username: "))
+        password = getpass.getpass()
+        client = Client(user, password)
+        return client
+    
+    def get_valid_queries(self):
+        """
+            Returns a list of tuples of valid threads. 
+            Each tuple is of the form (thread id, thread name)
+        """
+        valid_threads = []
+        
+        # Validate thread ids if thread ids are given.
+        if self.thread_ids:
+            for tid in self.thread_ids:
+                # Add thread name and its id if it is valid.
+                try:
+                    thread = self.client.fetchThreadInfo(tid)[tid]
+                    valid_threads.append((thread.uid, thread.name))
+            
+                # Ignore invalid ids.    
+                except FBchatException as e: 
+                    pass
 
-    args = parser.parse_args()
-    name = args.name
-    limit = int(args.limit)
+        # Validate names if names are given.
+        if self.names:
+            for name in self.names:
+                # Add thread name and its id if it is valid.
+                try:
+                    thread = self.client.searchForThreads(name)[0]
+                    valid_threads.append((thread.uid, thread.name))
+                
+                # Ignore invalid thread.    
+                except FBchatException as e: 
+                    pass
+            
+        if not len(valid_threads):
+            print("Error: No valid threads were found with the given queries")
+            exit(1)
 
-    return name, limit
+        return valid_threads     
 
-
-def login():
-    user = str(input("Username: "))
-    password = getpass.getpass()
-    client = Client(user, password)
-    return client
-
+    def download_messages(self):
+        query_threads = self.get_valid_queries()
+        print(query_threads)
 
 def process_message(message, user_id):
     if message.author == user_id or message == "None":
@@ -36,7 +78,7 @@ def main():
     user_id = client.uid
     thread = client.searchForThreads(search_name)[0]
 
-    # Gets the last 'limit' messages sent to the thread.
+    # Gets the last 'limit' number of messages sent to the conversation.
     print("Getting messages for {}...".format(thread.name))
     messages = client.fetchThreadMessages(thread_id=thread.uid, limit=limit)
 
@@ -59,4 +101,19 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    # Command line argument parsing.
+    parser = argparse.ArgumentParser(description='Downloads Facebook Messenger Convserations.')
+    parser.add_argument('--names', nargs='+', help='Names of user conversation to search for and get messages from.')
+    parser.add_argument('--ids', nargs='+', help='Messenger conversation ids to get messages from.')
+    parser.add_argument('--limit', default=30, help='Number of messages to be recorded')
+
+    args = parser.parse_args()
+
+    # Throw an error if no names or thread ids were supplied.
+    if not args.names and not args.ids:
+        print("Error: At least one of --names or --ids is required.")
+        parser.print_help()
+        exit(1)
+
+    md = MessageDownloader(args)
+    md.download_messages()
